c22077d2780463280a58fb8b17420e0080b6decf
Rudr Bansal <rudrabhishekbansal@gmail.com>
2022-12-30 23:00:35 +1100

    REFACTOR: Code optimisation
    Code optimisation by use of guard statement
    
    Closes PR comment

--- PokeDemo/Presenter/PokemonListPresenter.swift
+++ PokeDemo/Presenter/PokemonListPresenter.swift
@@ -45,24 +45,24 @@ final class PokemonListPresenter {
     
     func didSetupCellWith(pokemon: Pokemon, completion: @escaping (UIImage?) -> Void) {
         service.sendRequestWithJSON(endpoint: pokemon.url, method: .get) { response, error in
-            if error == nil {
-                do {
-                    guard let data = response as? Data else {
-                        completion(nil)
-                        return
-                    }
-                    let attributes = try JSONDecoder().decode(PokemonAttributes.self, from: data)
-                    let frontValue = attributes.attributes.frontImage
-                    let url = URL(string: frontValue)
-                    URLSession.shared.dataTask(with: url ?? URL(fileURLWithPath: "")) { data, response, error in
-                        guard let imageData = data else { return }
-                        completion(UIImage(data: imageData))
-                    }.resume()
-                }
-                catch {
+            guard error == nil else {
+                completion(nil)
+                return
+            }
+            do {
+                guard let data = response as? Data else {
                     completion(nil)
+                    return
                 }
-            } else {
+                let attributes = try JSONDecoder().decode(PokemonAttributes.self, from: data)
+                let frontValue = attributes.attributes.frontImage
+                let url = URL(string: frontValue)
+                URLSession.shared.dataTask(with: url ?? URL(fileURLWithPath: "")) { data, response, error in
+                    guard let imageData = data else { return }
+                    completion(UIImage(data: imageData))
+                }.resume()
+            }
+            catch {
                 completion(nil)
             }
         }
@@ -73,22 +73,17 @@ final class PokemonListPresenter {
     private func getPokemons() {
         service.sendRequestWithJSON(endpoint: Endpoints.shared.baseURL, method: .get) {[weak self] response, error in
             guard let self = self else { return }
-            if error == nil {
-                do {
-                    guard let pokemonResponse = response else { return }
-                    let pokemonResult = try JSONDecoder().decode(PokemonResult.self, from: pokemonResponse as! Data)
-                    self.delegate?.show(pokemonResult.results!)
-                }
-                catch {
-                    // Show alert that something is wrong with server
-                    self.delegate?.showAlert(title: Constants.error, message: error.localizedDescription)
-                }
-            } else if !Internet.shared.isAvailable() {
-                self.delegate?.showAlert(title: Constants.error, message: Internet.shared.noInternet)
-            } else {
-                // if error is network error - Show no internet connection alert
-                // else: Show general error alert
+            guard error == nil else {
                 self.delegate?.showAlert(title: Constants.error, message: error?.localizedDescription ?? "Sorry, something went wrong")
+                return
+            }
+            do {
+                guard let pokemonResponse = response else { return }
+                let pokemonResult = try JSONDecoder().decode(PokemonResult.self, from: pokemonResponse as! Data)
+                self.delegate?.show(pokemonResult.results!)
+            }
+            catch {
+                self.delegate?.showAlert(title: Constants.error, message: error.localizedDescription)
             }
         }
     }
