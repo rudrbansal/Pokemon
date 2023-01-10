9e271fe6ff907a5dd855bf9243bd101e331925fd
Rudr Bansal <rudrabhishekbansal@gmail.com>
2023-01-11 04:07:48 +1100

    REFACTOR: Change response from any to data
    Change response from any to data to avoid typecasting. Also, it avoids to receive any type of data
    
    Improves performance

--- PokeDemo/Screens/PokemonList/Model/Service.swift
+++ PokeDemo/Screens/PokemonList/Model/Service.swift
@@ -9,11 +9,11 @@ import Foundation
 import Alamofire
 
 protocol ServiceRepresentable {
-    func sendRequestWithJSON(endpoint: String, method: HTTPMethod, parameters: [String:Any]?, header: String?, _ onCompletion: @escaping ( _ response: Any?, _ error: Error?) -> Void)
+    func sendRequestWithJSON(endpoint: String, method: HTTPMethod, parameters: [String:Any]?, header: String?, _ onCompletion: @escaping ( _ responseData: Data?, _ error: Error?) -> Void)
 }
 
 extension ServiceRepresentable {
-    func sendRequestWithJSON(endpoint: String, method: HTTPMethod, parameters: [String:Any]? = nil, header: String? = nil, _ onCompletion: @escaping ( _ response: Any?, _ error: Error?) -> Void){
+    func sendRequestWithJSON(endpoint: String, method: HTTPMethod, parameters: [String:Any]? = nil, header: String? = nil, _ onCompletion: @escaping ( _ responseData: Data?, _ error: Error?) -> Void){
         self.sendRequestWithJSON(endpoint: endpoint, method: method, parameters: parameters, header: header, onCompletion)
     }
 }
@@ -25,9 +25,12 @@ final class Service: ServiceRepresentable {
     
     // MARK: - Exposed Methods
     
-    func sendRequestWithJSON(endpoint: String, method: HTTPMethod, parameters: [String:Any]? = nil, header: String? = nil, _ onCompletion: @escaping ( _ response: Any?, _ error: Error?) -> Void) {
+    func sendRequestWithJSON(endpoint: String, method: HTTPMethod, parameters: [String:Any]? = nil, header: String? = nil, _ onCompletion: @escaping ( _ responseData: Data?, _ error: Error?) -> Void) {
         AF.request(endpoint, method: method, parameters: parameters, encoding: URLEncoding.default, headers: nil, interceptor: nil).response { responseHandler in
-            guard let data = responseHandler.data else { return }
+            guard let data = responseHandler.data else {
+                onCompletion(nil, responseHandler.error)
+                return
+            }
             onCompletion(data, responseHandler.error)
         }
     }
